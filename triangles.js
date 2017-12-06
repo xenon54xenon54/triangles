ad37660d788f4045f1e7050b3ff87e26fd07771d
xenon54xenon54 <Ethan@Ethans-iMac.local>
2017-12-06 07:52:21 -0500

    organized again
    now there are 3 html files, bc 1 wasnt enough.

--- triangles.js
+++ triangles.js
@@ -7,25 +7,32 @@ function run() {
     document.getElementById("output").innerHTML = hypotenuse(leg1, leg2);
 }
 function classifyTri(a, b, c) {
-    //if ((a + b > c) && (a + c > b) && (b + c > a)) {
-        hypoLength = hypotenuse(a, b);
-        if (c > hypoLength) {
-            return ("Obtuse");
-        }
-        if (c == hypoLength) {
-            return ("Right");
-        }
-        if (c < hypoLength) {
-            return ("Acute");
-        }
+    hypoLength = hypotenuse(a, b);
+    if (c > hypoLength) {
+        return ("Obtuse");
     }
-    //else {
-    //   return ("Not a triangle");
-    //}
-//}
+    if (c == hypoLength) {
+        return ("Right");
+    }
+    if (c < hypoLength) {
+        return ("Acute");
+    }
+}
 function run2() {
     var leg1 = document.getElementById("classleg1").value;
     var leg2 = document.getElementById("classleg2").value;
     var hypotenuse = document.getElementById("classhypotenuse").value;
     document.getElementById("output2").innerHTML = classifyTri(leg1, leg2, hypotenuse);
+}
+function distBetween(a, b, x, y) {
+    var leg1 = a - x;
+    var leg2 = b - y;
+    return (hypotenuse(leg1, leg2));
+}
+function run3() {
+    var x1 = document.getElementById("x1").value;
+    var y1 = document.getElementById("y1").value;
+    var x2 = document.getElementById("x2").value;
+    var y2 = document.getElementById("y2").value;
+    document.getElementById("output3").innerHTML = distBetween(x1, y1, x2, y2);
 }
